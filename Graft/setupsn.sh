d4ca74a989d44c8c4760729a0f9718c1a6f01527
Fez29 <41666396+Fez29@users.noreply.github.com>
2019-03-28 23:27:56 +0200

    removed data dir step and added / in path

--- Graft/setupsn.sh
+++ Graft/setupsn.sh
@@ -5,7 +5,6 @@
 SN_NAME=$(whiptail --inputbox "SN Number - Folder directory to be created for SN - exclude numbering - script will detect next SN number in directory" 10 30 3>&1 1>&2 2>&3)
 Wallet=$(whiptail --inputbox "Mainnet Wallet Address - Enter Wallet address for staking - Ctrl-Shift-V or Shift + Insert to paste from clipboard" 10 30 3>&1 1>&2 2>&3)
 SN_PORT=$(whiptail --inputbox "SN Port - Port to be used for SN - must be unique per SN" 10 30 3>&1 1>&2 2>&3)
-DATA_DIR=$(whiptail --inputbox "Data Directory - Data Directory to be used (/home/User/SN Number/)" 10 30 3>&1 1>&2 2>&3)
 RPC_PORT=$(whiptail --inputbox "RPC port for Graftnoded - Mainnet Defailt = 18981 | Public Testnet Default = 28881" 10 30 3>&1 1>&2 2>&3)
 P2P_PORT=$(whiptail --inputbox "P2P port for Graftnoded - Mainnet Default = 18980 | Public Testnet Default = 28880" 10 30 3>&1 1>&2 2>&3)
 USER=$(whoami)
@@ -22,7 +21,7 @@ let n=$n+1
 SN="$SN_NAME$n"
 mkdir "$SN_NAME$n" &&
 cp $CURRENT_DIR/config.ini ~/"$SN"/config.ini &&
-sed -i "s|data_dir_var|/home/$USER/$SN|g" ~/"$SN"/config.ini &&
+sed -i "s|data_dir_var|/home/$USER/$SN/|g" ~/"$SN"/config.ini &&
 sed -i "s/wallet_var/$Wallet/g" ~/"$SN"/config.ini &&
 sed -i "s/rpc_port/$RPC_PORT/g" ~/"$SN"/config.ini &&
 sed -i "s/p2p_port/$P2P_PORT/g" ~/"$SN"/config.ini &&
